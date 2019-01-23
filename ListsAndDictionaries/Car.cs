f8587dcf0c7d249dbf3f971109153e26c0f95b34
Sabrina Andrew <sabrina.rae.andrew@gmail.com>
2019-01-23 18:28:01 -0500

    First Commit

---
+++ ListsAndDictionaries/Car.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ListsAndDictionaries
+{
+    public class Car
+    {
+        //This is a constructor
+        public Car(string make, string model, int year)
+        {
+            Make = make;
+            Model = model;
+            Year = year;
+        }
+        // End of constructor
+
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public int MilesPerGallon { get; set; }
+    }
+}
---
+++ ListsAndDictionaries/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ListsAndDictionaries
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var fordFiesta = new Car("Ford", "Fiesta", 2013);
+            var subarLegacy = new Car("Subaru", "Legacy", 2017);
+            var hondaCrv = new Car("Honda", "CRV", 2014);
+
+            var parkedCars = new Car[5];
+            Console.WriteLine(parkedCars.Length);
+
+            // Puts Honda CRV in array
+            parkedCars[0] = hondaCrv;
+
+            var firstCar = parkedCars[0];
+            Console.WriteLine(firstCar.Make);
+
+
+            //List instead of array
+
+            //Angle brackets are generics.  
+            var luxuryCars = new List<Car>();
+            var firstLuxuryCar = luxuryCars[0];
+
+            luxuryCars[0] = hondaCrv;
+
+            //Another way to add to a list
+            luxuryCars.Add(firstLuxuryCar);
+
+            Console.WriteLine(firstLuxuryCar);
+
+
+            Console.ReadLine();
+        }
+    }
+}
